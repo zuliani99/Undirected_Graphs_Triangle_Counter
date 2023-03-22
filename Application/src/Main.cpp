83f7ea508a7c9401a0b40060d16d338a187431ce
Riccardo Zuliani <riccardo.zuliani99@gmail.com>
2023-03-22 18:03:52 +0100

    random graphs

--- Application/src/Main.cpp
+++ Application/src/Main.cpp
@@ -74,8 +74,8 @@ int main() {
     stream << "name,n_edges,n_vertices,density,threads,n_triangles,elapsed_triangle_count,speed_up_traingle_count,elapsed_adjacent_list,speed_up_adjacent_list\n";
     stream.close();
 
-    //RunGraphs(random_datasets_path, results_path);
     RunGraphs(standford_datasets_path, results_path);
+    RunGraphs(random_datasets_path, results_path);
 
     return 0;
 }
